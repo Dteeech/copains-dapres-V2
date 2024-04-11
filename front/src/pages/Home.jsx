60ef32493e3314e0df4047b1f0c1a533c8add68e
Dteeech <marshall.isaac@protonmail.com>
2024-04-11 16:03:13 +0200

    added the search bar layout : todo algorythm logic and refacto

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Input from '../components/forms/inputs/Input'
+import searchIcon from '../assets/images/icons/searchIcon.svg'
 function Home () {
   return (
     <>
@@ -11,7 +11,10 @@ function Home () {
           créez des souvenirs
         </h1>
       </div>
-      <input className='shadow appearance-none border rounded-3xl h-12 bg-gray-50 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='text' name='' id='' />
+      <div className=' relative container w-1/4 flex justify-center m-auto my-16'>
+        <img className='absolute left-6 top-3' src={searchIcon} alt='' />
+        <input placeholder='colocation nantes pour 4' className='placeholder:text-center shadow appearance-none border rounded-full h-14 bg-gray-50 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type='text' name='' id='' />
+      </div>
       <div className='' />
     </>
 
