10d808dd729360030e4b8c6563e0402518551c03
Isaac Marshall <isaacmarshall@Air-de-Isaac.lan>
2024-05-25 13:52:38 +0200

    style chages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,8 +4,8 @@ function Footer () {
   return (
     <footer className=' w-full text-white h-30 text-start absolute left-0 flex gap-10 bg-primary-500 py-6 px-10'>
       <div className=' container flex flex-col  align-top w-1/4'>
-        <h2 className='border-b-2 border-white'>Liens</h2>
-        <nav className=' flex flex-col'>
+        <h3 className='border-b-2 text-2xl py-4 border-white'>Liens</h3>
+        <nav className=' flex flex-col mt-4'>
           <a href='/accueil'>accueil</a>
           <a href='/annonces'>Annonce</a>
           <a href='/favoris'>Favoris</a>
@@ -18,8 +18,8 @@ function Footer () {
         </nav>
       </div>
       <div className='flex flex-col w-1/4 h-max'>
-        <h2 className='border-b-2 border-white '>Informations légales</h2>
-        <nav className=' flex flex-col'>
+        <h3 className='border-b-2 text-2xl py-4 border-white '>Informations légales</h3>
+        <nav className=' flex flex-col mt-4'>
           <a href='/cookies'>Cookies</a>
           <a href='/policie'>Politique de confidentialité</a>
           <a href='/conditions-generales'>Conditions générales</a>
@@ -27,8 +27,8 @@ function Footer () {
         </nav>
       </div>
       <div className='flex flex-col w-1/4 h-max'>
-        <h2 className='border-b-2 border-white'>Besoin d'informations</h2>
-        <nav className=' flex flex-col h-max'>
+        <h3 className='border-b-2 text-2xl py-4 border-white'>Besoin d'informations</h3>
+        <nav className=' flex flex-col mt-4 h-max'>
           <a href='/faq'>FAQ</a>
           <a href='/plan-site'>Plan du site</a>
           <a href='/contact'>Contact</a>
@@ -36,8 +36,8 @@ function Footer () {
         </nav>
       </div>
       <div className='flex flex-col w-1/4'>
-        <h2 className='border-b-2 border-white'>Retrouvez-nous sur</h2>
-        <nav className=' flex flex-col'>
+        <h3 className='border-b-2 text-2xl py-4 border-white'>Retrouvez-nous sur</h3>
+        <nav className=' flex flex-col mt-4'>
           <a href='/faq'>FAQ</a>
           <a href='/plan-site'>Plan du site</a>
           <a href='/contact'>Contact</a>
